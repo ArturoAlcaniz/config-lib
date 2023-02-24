ad37c4f406d2fb1c386ee1caa0d42282790f166e
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-02-24 22:42:12 +0000

    separate to own repository service

--- entities-lib/src/entities/categoryProduct.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum Category {
-    WEB_APPLICATION = "Web application",
-    LICENSE = "License",
-    DESKTOP_APPLICATION = "Desktop application",
-    MODULE = "Module",
-    CMS = "CMS",
-    STYLES = "Styles",
-    NODE_PACKAGE = "Node package"
-}
--- entities-lib/src/transformers/EntityWrapper.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntity } from "typeorm";
-
-export abstract class EntityWrapper extends BaseEntity {
-    toString(): string {
-        return JSON.stringify(this);
-    }
-}
